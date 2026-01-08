68254463ab9797604f1173cf9ab060f08a9f4090
Gregory909 <124597526+DarkCheatClientTeam@users.noreply.github.com>
2026-01-08 22:17:22 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,2 @@
+getgenv().SCRIPT_KEY = "";
 loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/1489262dd4b5ed8795e5c18c2f8bf690cfaa0d50455394974220a2053903b777/download"))()
