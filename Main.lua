33c754f6c64c83b40fa346b89b3b356a1d6d5017
Gregory909 <124597526+DarkCheatClientTeam@users.noreply.github.com>
2026-01-08 22:15:51 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Script has shutted down for short time. Please wait for back up!")
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/1489262dd4b5ed8795e5c18c2f8bf690cfaa0d50455394974220a2053903b777/download"))()
