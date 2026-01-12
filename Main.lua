8f9c2519a7cba7b382dfda2860f5bf58bc43803a
Gregory909 <124597526+DarkCheatClientTeam@users.noreply.github.com>
2026-01-12 18:29:57 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 local success, error = pcall(function()
     getgenv().SCRIPT_KEY = "";
-    loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/1489262dd4b5ed8795e5c18c2f8bf690cfaa0d50455394974220a2053903b777/download"))()
+    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/1489262dd4b5ed8795e5c18c2f8bf690cfaa0d50455394974220a2053903b777/download"))()
 end)
 if success then
   print("[DARKCHEATCLIENT] Success! Loading script...")
